ad2f6011d4b9cbea0197c2c568704a7027460995
runbmp <9081154+runbmp@users.noreply.github.com>
2021-04-07 09:23:32 -0500

    Create archinstall.sh

---
+++ archinstall.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+ip link
+
+ping google.com -c 1
+
+reflector --country US --latest 100 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+timedatectl set-ntp true
+
+fdisk -l
+
+echo "enter the disk you wish to blow away and install arch to"
+read -r DISKDEV
+
+sgdisk -og "$DISKDEV"
+sgdisk -n 1:0:+500M -t 1:ef00 "$DISKDEV"
+sgdisk -n 2:0:0 "$DISKDEV"
+sgdisk -p "$DISKDEV"
+
+mkfs.fat -F32 "$DISKDEV"1
+mkfs.btrfs "$DISKDEV"2
+
+mount "$DISKDEV"2 /mnt
+cd /mnt || echo "couldn't cd into /mnt" && return
+
+btrfs subvolume create @
+btrfs subvolume create @home
+btrfs subvolume create @snapshots
+btrfs subvolume create @var_log
+cd || echo "couldn't cd into /" && return
+
+umount /mnt
+mount -o noatime,compress=lzo,space_cache=v2,discard=async,subvol=@ "$DISKDEV"2 /mnt
+mkdir -p /mnt/{boot/efi,home,.snapshots,var/log}
+mount -o noatime,compress=lzo,space_cache=v2,discard=async,subvol=@home "$DISKDEV"2 /mnt/home
+mount -o noatime,compress=lzo,space_cache=v2,discard=async,subvol=@snapshots "$DISKDEV"2 /mnt/.snapshots
+mount -o noatime,compress=lzo,space_cache=v2,discard=async,subvol=@var_log "$DISKDEV"2 /mnt/var/log
+mount "$DISKDEV"1 /mnt/boot/efi
+
+pacstrap /mnt base base-devel linux linux-firmware linux-lts nano btrfs-progs grub efibootmgr git networkmanager dialog
