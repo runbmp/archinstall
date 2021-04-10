b1d1549a35d3180a169cb63a11e1c76fed2ec3e7
runbmp <9081154+runbmp@users.noreply.github.com>
2021-04-10 14:57:20 -0500

    Update archchrootinstall.sh

--- archchrootinstall.sh
+++ archchrootinstall.sh
@@ -31,7 +31,7 @@ swapon swapfile
 
 echo "/n" > /etc/fstab
 echo "# /dev/nvme0n1p5/n" > /etc/fstab
-echo "/swapfile none swap defaults 0 0" > /etc/fstab
+echo "/swap/swapfile none swap defaults 0 0" > /etc/fstab
 cat /etc/fstab
 
 statusprint "set timezone in /etc/localtime"
@@ -54,31 +54,31 @@ echo "\
 127.0.0.1	localhost
 ::1		localhost
 127.0.1.1	$MYHOST.localdomain	$MYHOST
-"
+" >> /etc/hosts
 
 statusprint "run mkinitcpio for all installed kernels"
 #todo this is only for partitionless install?
 #sed -i 's/^MODULES()/MODULES(btrfs)/' /etc/mkinitcpio.conf
+sed -i 's/^MODULES()/MODULES(amdgpu)/' /etc/mkinitcpio.conf
 mkinitcpio -P
 
-statusprint "install refind bootloader"
-refind-install
+statusprint "install grub bootloader"
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
 
 statusprint "setup root password" "1;33"
 passwd
 
 statusprint "setup new user ben password and add wheel as sudoer" "1;33"
 useradd -mG wheel ben
+usermod -a -G video ben
 passwd ben
 sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
 statusprint "grab dotfiles and bootstrap them"
 sudo -u ben sh -c 'cd "$HOME" && echo ".dotfiles" >> .gitignore'
-sudo -u ben sh -c 'cd "$HOME" && git clone https://github.com/runbmp/dotfiles.git "$HOME/.dotfiles"'
-sudo -u ben sh -c 'cd "$HOME" && /usr/bin/git --git-dir="$HOME/.dotfiles/.git" --work-tree="$HOME" config --local status.showUntrackedFiles no'
-sudo -u ben sh -c 'cd "$HOME" && /usr/bin/git --git-dir="$HOME/.dotfiles/.git" --work-tree="$HOME" checkout master'
-sudo -u ben sh -c 'cd "$HOME" && /usr/bin/git --git-dir="$HOME/.dotfiles/.git" --work-tree="$HOME" pull'
-sudo -u ben sh -c 'cd "$HOME" && /usr/bin/git --git-dir="$HOME/.dotfiles/.git" --work-tree="$HOME" restore .'
+sudo -u ben sh -c 'cd "$HOME" && git clone --bare https://github.com/runbmp/dotfiles.git "$HOME"/.dotfiles'
+sudo -u ben sh -c 'cd "$HOME" && /usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" checkout'
 
 sudo -u ben sh -c 'cd "$HOME" && chmod +x "$HOME"/.bootstrap.sh'
 sudo -u ben sh -c 'cd "$HOME" && "$HOME"/.bootstrap.sh'
